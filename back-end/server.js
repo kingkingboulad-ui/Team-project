68a96358914445028de622c159926d96d750e5a8
abedlrazak boulad <abedboulad@gmail.coom>
2026-09-08 18:38:17 +0300

    create front end

--- back-end/server.js
+++ back-end/server.js
@@ -5,7 +5,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-const testgit = "goog"
+
+
 
 app.listen(PORT , ()=>{
     console.log(`Server is running on port ${PORT}`)
